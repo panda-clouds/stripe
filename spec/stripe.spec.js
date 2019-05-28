47a3506cd55a0c425e066a7213a642c823f86379
Marc Smith <mr.marcsmith@me.com>
2019-05-28 16:15:32 -0700

    added APIKeys

--- spec/stripe.spec.js
+++ spec/stripe.spec.js
@@ -1,6 +1,13 @@
 
 const PCStripe = require('../src/PCStripe.js');
 
+try {
+	require('../apiKeys.js')();
+} catch (e) {
+	// It's ok if we don't load the keys from the apiKeys file
+	// In CI we load directly
+}
+
 describe('test OAuth', () => {
 	it('should ', () => {
 		expect.assertions(1);
