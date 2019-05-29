35fe9521b915fe0cd21bc05afd470da0056f1b3d
Marc Smith <mr.marcsmith@me.com>
2019-05-29 14:23:36 -0700

    added OAuth instructions

--- spec/stripe.spec.js
+++ spec/stripe.spec.js
@@ -8,6 +8,14 @@ try {
 	// In CI we load directly
 }
 
+// The OAuth Workflow requires UI intervention
+// Follow these steps to generate an auth Code
+//
+// 1. go to https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_F9qAHcnb9ICmtKU8Ra6JLX3LXgdGDKKe&scope=read_write
+// 2. in the top banner press "Skip this account form"
+// 3. replace the "auth_code" variable below v
+// 4. run the tests
+
 describe('test OAuth', () => {
 	const auth_code = 'ac_F9sFFZHsRkxIKT0hAX5TzHozsRWHHGlo';
 	const client_secret = 'sk_test_hPTcgGfDuH0frQtuABpPXloO';
