b23530d56222ea976ef651595215a9493728a9ff
east-empire <42353905+east-empire@users.noreply.github.com>
2019-06-10 14:54:23 -0700

    Dev (#1)
    * writing tests.
    
    * added async
    
    * added expect to spec
    
    * added OAuth instructions
    
    * Update stripe.spec.js
    
    * getUserIdFromStripe now passes
    
    * added step 1 of the oauth flow to PCStripe.
    
    * added transfer to connected account.
    
    * changed to passing in the secret key to constuctor
    
    * insufficient funds
    
    * fixed spec
    
    * ignoring jest_0

--- spec/stripe.spec.js
+++ spec/stripe.spec.js
@@ -1,5 +1,6 @@
 
 const PCStripe = require('../src/PCStripe.js');
+const myStripe = new PCStripe('sk_test_hPTcgGfDuH0frQtuABpPXloO');
 
 try {
 	require('../apiKeys.js')();
@@ -8,10 +9,54 @@ try {
 	// In CI we load directly
 }
 
+// The OAuth Workflow requires UI intervention
+// Follow these steps to generate an auth Code
+//
+// 1. go to
+// https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_F9qAHcnb9ICmtKU8Ra6JLX3LXgdGDKKe&scope=read_write
+// 2. in the top banner press "Skip this account form"
+// 3. grab the "ac_bla" token from the page and replace the "auth_code" variable below v
+// 4. run the tests
+
 describe('test OAuth', () => {
-	it('should ', () => {
+	// const auth_code = 'ac_FBiuWezs8tNG2doKWIZEYc9E720ctf9L';
+	const refreshToken = 'rt_FENTYwbxoKbaLMsPtfQmrNbJzeAvXd2qyOVkUh2AzGbQigzU';
+	let account_id;
+
+	it('should create the oauth link', async () => {
 		expect.assertions(1);
-		expect(3).toBe(3);
-		PCStripe.pass();
+
+		const join_link = await PCStripe.getStripeOAuthLink();
+
+		expect(join_link).toContain('scope=read_write');
+	});
+
+	// Test cannot run with stripes system without human interviention and is disabled for production test suites.
+	it('should fetch credentials from stripe', async () => {
+		expect.assertions(7);
+		const response = await myStripe.getUserIdFromStripeRefresh(refreshToken);
+
+		console.log(response);
+
+		expect(response.access_token).toContain('sk_test_');
+		expect(response.livemode).toBe(false);
+		expect(response.refresh_token).toContain('rt_');
+		expect(response.token_type).toBe('bearer');
+		expect(response.stripe_publishable_key).toContain('pk_test_');
+		expect(response.stripe_user_id).toContain('acct_');
+		account_id = response.stripe_user_id;
+		expect(response.scope).toBe('read_write');
+	});
+
+	it('should make transfers', async () => {
+		expect.assertions(2);
+
+		// comment
+		const transfer = await myStripe.transferToConnectedAccount(account_id, 350, 'parent_job_id');
+
+		// transfer is
+		// {"amount": 350, "amount_reversed": 0, "balance_transaction": "txn_1Ejv2GL54TAiok0DXZAFspzV", "created": 1560202092, "currency": "usd", "description": null, "destination": "acct_1EjuhUKT5Kn7XEO2", "destination_payment": "py_1Ejv2HKT5Kn7XEO2pQWHYKQT", "id": "tr_1Ejv2GL54TAiok0DP8DMglnZ", "livemode": false, "metadata": {}, "object": "transfer", "reversals": {"data": [], "has_more": false, "object": "list", "total_count": 0, "url": "/v1/transfers/tr_1Ejv2GL54TAiok0DP8DMglnZ/reversals"}, "reversed": false, "source_transaction": null, "source_type": "card", "transfer_group": "parent_job_id"}
+		expect(transfer.amount).toBe(350);
+		expect(transfer.balance_transaction).toContain('txn_');
 	});
 });
--- src/PCStripe.js
+++ src/PCStripe.js
@@ -1,9 +1,65 @@
 class PCStripe {
-	constructor() {
-		// Empty Constructor
+	constructor(secret) {
+		this.secretKey = secret;
+		this.stripe = require('stripe')(this.secretKey);
+		// Pass the stripe secret key
 	}
 
 	static pass() {
+	}
+
+	static getStripeOAuthLink(clientId) {
+		let link = 'https://connect.stripe.com/oauth/authorize?response_type=code?client_id=' + clientId;
+
+		// set scope
+		link += '&scope=read_write';
+
+		return link;
+	}
+
+	async getUserIdFromStripe(auth_code) {
+		const request = require('request-promise');
+		const options = {
+			method: 'POST',
+			json: true,
+			headers: {},
+			uri: 'https://connect.stripe.com/oauth/token',
+			body: { client_secret: this.secretKey,
+				code: auth_code,
+				grant_type: 'authorization_code' },
+		};
+
+		const httpResponse = await request(options);
+
+		return httpResponse;
+	}
+
+	async getUserIdFromStripeRefresh(refresh) {
+		const request = require('request-promise');
+		const options = {
+			method: 'POST',
+			json: true,
+			headers: {},
+			uri: 'https://connect.stripe.com/oauth/token',
+			body: { client_secret: this.secretKey,
+				refresh_token: refresh,
+				grant_type: 'refresh_token' },
+		};
+
+		const httpResponse = await request(options);
+
+		return httpResponse;
+	}
+
+	async transferToConnectedAccount(dest_acct_id, payment_amount, group_id) {
+		const transfer = await this.stripe.transfers.create({
+			amount: payment_amount,
+			currency: 'usd',
+			destination: dest_acct_id,
+			transfer_group: group_id,
+		});
+
+		return transfer;
 	}
 }
 
